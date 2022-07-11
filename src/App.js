eb64c15f164dd8d8b52495431195b113e72da2d7
anamarincic <amarincic5@gmail.com>
2022-07-11 19:59:35 +0200

    Add components-itemform

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { ItemForm } from "./components/ItemForm";
 
 function App() {
   return (
-    <div className="App">
-      todo-app
+    <div>
+     <div>
+       <form>
+         <input type="text" />
+         <button type="submit">+</button>
+       </form>
+     </div>
+     <div>
+       <div>
+         <input type="checkbox" name="" id="" />
+         <span>#item 1</span>
+         <button type="button">Delete</button>
+       </div>
+     </div>
     </div>
   );
 }
